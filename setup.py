83f9fe0b75f548981632b9674ac27604cc39b23a
Robert Valta <robert.j.valta@gmail.com>
2018-10-01 13:15:05 +0300

    Add python package dependencies to setup

--- setup.py
+++ setup.py
@@ -14,4 +14,17 @@ setuptools.setup(
         "Operating System :: OS Independent", "Programming Language :: Python"
     ],
     packages=setuptools.find_packages(),
+    install_requires=[
+        "devp2p==0.9.3",
+        "hypothesis",
+        "ipfsapi==0.4.2.post1",
+        "mypy",
+        "py-solc==3.0.0",
+        "schematics",
+        "secp256k1==0.13.2",
+        "web3==v4.2.0",
+    ],
+    dependency_links=[
+        "git://github.com/mfranciszkiewicz/pyelliptic.git@1.5.10#egg=pyelliptic",
+    ]
 )
