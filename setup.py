1047cea08767e7ed4d3f0ca428f1355aa35a36da
Robert Valta <robert.j.valta@gmail.com>
2018-10-01 11:36:12 +0300

    Update setup file

--- setup.py
+++ setup.py
@@ -1,26 +1,12 @@
-#   Copyright 2011-2016 Josh Kearney
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-
 import setuptools
 
 setuptools.setup(
     name="human-contracts",
     version=.1,
-    author="Alex Newman",
+    author="HUMAN Protocol",
     description=
-    "A python interface to interact with The Human Contracts smart bounties and erc20 token",
-    url="https://github.com/IntuitionMachines/hmt-contracts",
+    "A python library to launch escrow contracts to the HUMAN network.",
+    url="https://github.com/hCaptcha/hmt-escrow",
     include_package_data=True,
     zip_safe=True,
     classifiers=[
