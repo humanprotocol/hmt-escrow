862d37ebcb64ccf8123157877dff8ae783abf795
Robert Valta <robert.j.valta@gmail.com>
2018-12-10 16:02:34 +0200

    Add HMTokenInterface and HMToken

--- hmt.sol
+++ hmt.sol
@@ -66,11 +66,21 @@ library SafeMath {
 }
 
 
-contract EIP20Interface {
+contract HMTokenInterface {
+    /* This is a slight change to the ERC20 base standard.
+    function totalSupply() constant returns (uint256 supply);
+    is replaced with:
+    uint256 public totalSupply;
+    This automatically creates a getter function for the totalSupply.
+    This is moved to the base contract since public getter functions are not
+    currently recognised as an implementation of the matching abstract
+    function by the compiler.
+    */
     /// total amount of tokens
     uint256 public totalSupply;
 
-    function fund() public payable returns(bool success) { }  // solhint-disable-line no-empty-blocks
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
     /// @param _owner The address from which the balance will be retrieved
     /// @return The balance
@@ -99,12 +109,152 @@ contract EIP20Interface {
     /// @param _spender The address of the account able to transfer the tokens
     /// @return Amount of remaining tokens allowed to spent
     function allowance(address _owner, address _spender) public view returns (uint256 remaining);
+}
 
-    /* solhint-disable no-simple-event-func-name */
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    /* solhint-enable no-simple-event-func-name */
+contract HMToken is HMTokenInterface {
+    using SafeMath for uint256;
+    uint256 private constant MAX_UINT256 = 2**256 - 1;
+    uint256 private constant BULK_MAX_VALUE = 1000000000 * (10 ** 18);
+    uint32  private constant BULK_MAX_COUNT = 100;
 
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event BulkTransfer(uint256 indexed _txId, uint256 _bulkCount);
+    event BulkApproval(uint256 indexed _txId, uint256 _bulkCount);
+
+    mapping (address => uint256) private balances;
+    mapping (address => mapping (address => uint256)) private allowed;
+
+    string public name;
+    uint8 public decimals;
+    string public symbol;
+
+    constructor(uint256 _totalSupply, string _name, uint8 _decimals, string _symbol) public {
+        totalSupply = _totalSupply * (10 ** uint256(_decimals));
+        name = _name;
+        decimals = _decimals;
+        symbol = _symbol;
+        balances[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        success = transferQuiet(_to, _value);
+        require(success, "Transfer didn't succeed");
+        return success;
+    }
+
+    function transferFrom(address _spender, address _to, uint256 _value) public returns (bool success) {
+        uint256 _allowance = allowed[_spender][msg.sender];
+        require(balances[_spender] >= _value && _allowance >= _value, "Spender balance or allowance too low");
+        require(_to != address(0), "Can't send tokens to uninitialized address");
+
+        balances[_spender] = balances[_spender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+
+        if (_allowance < MAX_UINT256) { // Special case to approve unlimited transfers
+            allowed[_spender][msg.sender] = allowed[_spender][msg.sender].sub(_value);
+        }
+
+        emit Transfer(_spender, _to, _value);
+        return true;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        require(_spender != address(0), "Token spender is an uninitialized address");
+        
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value); //solhint-disable-line indent, no-unused-vars
+        return true;
+    }
+
+    function increaseApproval(address _spender, uint _delta) public returns (bool success) {
+        require(_spender != address(0), "Token spender is an uninitialized address");
+        
+        uint _oldValue = allowed[msg.sender][_spender];
+        if (_oldValue.add(_delta) < _oldValue || _oldValue.add(_delta) >= MAX_UINT256) { // Truncate upon overflow.
+            allowed[msg.sender][_spender] = MAX_UINT256.sub(1);
+        } else {
+            allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_delta);
+        }
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+
+    function decreaseApproval(address _spender, uint _delta) public returns (bool success) {
+        require(_spender != address(0), "Token spender is an uninitialized address");
+        
+        uint _oldValue = allowed[msg.sender][_spender];
+        if (_delta > _oldValue) { // Truncate upon overflow.
+            allowed[msg.sender][_spender] = 0;
+        } else {
+            allowed[msg.sender][_spender] = allowed[msg.sender][_spender].sub(_delta);
+        }
+        emit Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+
+    function transferBulk(address[] _tos, uint256[] _values, uint256 _txId) public returns (uint256 _bulkCount) {
+        require(_tos.length == _values.length, "Amount of recipients and values don't match");
+        require(_tos.length < BULK_MAX_COUNT, "Too many recipients");
+
+        uint256 _bulkValue = 0;
+        for (uint j = 0; j < _tos.length; ++j) {
+            _bulkValue = _bulkValue.add(_values[j]);
+        }
+        require(_bulkValue < BULK_MAX_VALUE, "Bulk value too high");
+
+        _bulkCount = 0;
+        bool _success;
+        for (uint i = 0; i < _tos.length; ++i) {
+            _success = transferQuiet(_tos[i], _values[i]);
+            if (_success) {
+                _bulkCount = _bulkCount.add(1);
+            }
+        }
+        emit BulkTransfer(_txId, _bulkCount);
+        return _bulkCount;
+    }
+
+    function approveBulk(address[] _spenders, uint256[] _values, uint256 _txId) public returns (uint256 _bulkCount) {
+        require(_spenders.length == _values.length, "Amount of spenders and values don't match");
+        require(_spenders.length < BULK_MAX_COUNT, "Too many spenders");
+
+        uint256 _bulkValue = 0;
+        for (uint j = 0; j < _spenders.length; ++j) {
+            _bulkValue = _bulkValue.add(_values[j]);
+        }
+        require(_bulkValue < BULK_MAX_VALUE, "Bulk value too high");
+
+        _bulkCount = 0;
+        bool _success;
+        for (uint i = 0; i < _spenders.length; ++i) {
+            _success = increaseApproval(_spenders[i], _values[i]);
+            if (_success) {
+                _bulkCount = _bulkCount.add(1);
+            }
+        }
+        emit BulkApproval(_txId, _bulkCount);
+        return _bulkCount;
+    }
+
+    // Like transfer, but fails quietly.
+    function transferQuiet(address _to, uint256 _value) internal returns (bool success) {
+        if (_to == address(0)) return false; // Preclude burning tokens to uninitialized address.
+        if (_to == address(this)) return false; // Preclude sending tokens to the contract.
+        if (balances[msg.sender] < _value) return false;
+
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
 }
 
 
@@ -187,12 +337,12 @@ contract Escrow {
     }
 
     function getBalance() public view returns (uint256) {
-        return EIP20Interface(eip20).balanceOf(address(this));
+        return HMTokenInterface(eip20).balanceOf(address(this));
     }
 
     function getAddressBalance(address _address) public view returns (uint256) {
         require(_address != address(0), "Token spender is an uninitialized address");
-        return EIP20Interface(eip20).balanceOf(address(_address));
+        return HMTokenInterface(eip20).balanceOf(address(_address));
     }
 
     function getReputationOracle() public view returns (address) {
@@ -334,7 +484,7 @@ contract Escrow {
     }
 
     function partialPayout(uint256 _amount, address _recipient) internal returns (bool) {
-        EIP20Interface token = EIP20Interface(eip20);
+        HMTokenInterface token = HMTokenInterface(eip20);
         uint256 reputationOracleFee = reputationOracleStake.mul(_amount).div(100);
         uint256 recordingOracleFee = recordingOracleStake.mul(_amount).div(100);
         uint256 amount = _amount.sub(reputationOracleFee).sub(recordingOracleFee);
