74be8235e7268b9fe1af59d2a6466df17ac754d1
Robert Valta <robert.j.valta@gmail.com>
2018-12-12 21:30:36 +0200

    Bump to 0.3.0 due to major changes to contract level code due to bulk payments

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="hmt-escrow",
-    version="0.2.4",
+    version="0.3.0",
     author="HUMAN Protocol",
     description=
     "A python library to launch escrow contracts to the HUMAN network.",
