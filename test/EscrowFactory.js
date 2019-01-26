2c5053b22872f31fc32228ff0da977c3bedea4f1
Jan Antensteiner <jan.antensteiner@gmail.com>
2019-01-27 00:02:12 +0200

    Add describe blocks to js tests

--- test/EscrowFactory.js
+++ test/EscrowFactory.js
@@ -18,32 +18,33 @@ contract('EscrowFactory', (accounts) => {
       assert(false);
     }
   });
+  describe('calling createEscrow', () => {
+    it('creates new escrow contract', async () => {
+      try {
+        const escrow = await EscrowFactory.createEscrow({ from: accounts[0] });
+        assert(escrow);
+      } catch (ex) {
+        assert(false);
+      }
+    });
 
-  it('creates new escrow contract when calling createEscrow', async () => {
-    try {
-      const escrow = await EscrowFactory.createEscrow({ from: accounts[0] });
-      assert(escrow);
-    } catch (ex) {
-      assert(false);
-    }
+    it('increases counter after new escrow is created', async () => {
+      try {
+        const initialCounter = await EscrowFactory.getCounter();
+        assert.equal(initialCounter.toNumber(), 0);
+
+        await EscrowFactory.createEscrow({ from: accounts[0] });
+
+        const counterAfterFirstEscrow = await EscrowFactory.getCounter();
+        assert.equal(counterAfterFirstEscrow.toNumber(), 1);
+
+        await EscrowFactory.createEscrow({ from: accounts[0] });
+
+        const counterAfterSecondEscrow = await EscrowFactory.getCounter();
+        assert.equal(counterAfterSecondEscrow.toNumber(), 2);
+      } catch (ex) {
+        assert(false);
+      }
+    });
   });
-
-  it('increases counter when creating a new escrow', async () => {
-    try {
-      initialCounter = await EscrowFactory.getCounter();
-      assert.equal(initialCounter.toNumber(), 0);
-
-      await EscrowFactory.createEscrow({ from: accounts[0] });
-
-      counterAfterFirstEscrow = await EscrowFactory.getCounter();
-      assert.equal(counterAfterFirstEscrow.toNumber(), 1);
-
-      await EscrowFactory.createEscrow({ from: accounts[0] });
-
-      counterAfterSecondEscrow = await EscrowFactory.getCounter();
-      assert.equal(counterAfterSecondEscrow.toNumber(), 2);
-    } catch (ex) {
-      assert(false);
-    }
-  })
 });
