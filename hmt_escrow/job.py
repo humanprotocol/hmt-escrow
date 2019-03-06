2ee2013720863e96724bca25a390fcab5f01fdea
Robert Valta <robert.j.valta@gmail.com>
2019-03-06 15:53:41 +0200

    Remove unused FACTORY_ADDR env variable

--- hmt_escrow/job.py
+++ hmt_escrow/job.py
@@ -16,7 +16,6 @@ from storage import download, upload
 from basemodels import Manifest
 
 GAS_LIMIT = int(os.getenv("GAS_LIMIT", 4712388))
-FACTORY_ADDR = os.getenv("FACTORY_ADDR")
 
 LOG = logging.getLogger("hmt_escrow.job")
 Status = Enum('Status', 'Launched Pending Partial Paid Complete Cancelled')
