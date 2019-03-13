323132e751eafab034323d14b1036927e9c7134c
Evin Callahan <evin.callahan@gmail.com>
2019-03-13 15:05:34 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="hmt-escrow",
-    version="0.5.0",
+    version="0.5.1",
     author="HUMAN Protocol",
     description=
     "A python library to launch escrow contracts to the HUMAN network.",
