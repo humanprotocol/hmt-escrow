b90917d892df9784ef2b479fda9e7daef6f51e74
Evin Callahan <gaieges@gmail.com>
2019-03-13 16:01:54 -0500

    Bump lock file (#81)
    * Bump lock file
    
    * bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="hmt-escrow",
-    version="0.5.1",
+    version="0.5.2",
     author="HUMAN Protocol",
     description=
     "A python library to launch escrow contracts to the HUMAN network.",
