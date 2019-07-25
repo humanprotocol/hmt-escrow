884042e6b85151c00883934d03bb398d9077acac
Robert Valta <robert.j.valta@gmail.com>
2019-07-25 14:31:59 +0300

    Bump truffle to fix diff sec vulnerability

--- truffle.js
+++ truffle.js
@@ -28,4 +28,17 @@ module.exports = {
       network_id: '4',
     },
   },
+  compilers: {
+    solc: {
+      version: "0.4.24", // Fetch exact version from solc-bin (default: truffle's version)
+      settings: {
+        // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: false,
+          runs: 200
+        },
+        evmVersion: "byzantium"
+      }
+    }
+  }
 };
