116dbd9ace35b19111cedf177f342496019a3385
Nick Geoca <nickgeoca@gmail.com>
2019-11-12 12:14:38 -0800

    change port to match docker

--- truffle.js
+++ truffle.js
@@ -7,7 +7,7 @@ module.exports = {
   networks: {
     development: {
       host: ETH_HOST || '127.0.0.1',
-      port: ETH_PORT || 9545,
+      port: ETH_PORT || 8545,
       network_id: '*',
     },
     live: {
