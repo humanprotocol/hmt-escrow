fa00eed039251d2223b25fefddea87580018a739
Nick Geoca <nickgeoca@gmail.com>
2019-11-12 13:18:47 -0800

    turned on code optimizer

--- truffle.js
+++ truffle.js
@@ -34,7 +34,7 @@ module.exports = {
       settings: {
         // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
-          enabled: false,
+          enabled: true,
           runs: 200
         },
         evmVersion: "byzantium"
