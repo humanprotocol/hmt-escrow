66a41645ff5390f5863f8175775605cd56df5390
Nick Geoca <nickgeoca@gmail.com>
2019-12-11 17:37:18 -0800

    Mock ipfs test

--- hmt_escrow/test_ipns.py
+++ hmt_escrow/test_ipns.py
@@ -1,55 +1,34 @@
 import unittest
-import logging
 import mock
-from mock import patch
-from basemodels import Manifest
-from loader.entry import runner
-from loader.blockchain import load_job_blockchain
-from common_helpers import mocked_exchange_post, load_fake_manifest, mock_job, mock_get_pk_from_address, set_up_blockchain_config
-
+# from mock import patch
 
 class IpnsTest(unittest.TestCase):
     '''
-    1) apply patch on X, 
-    2) call function that in turn calls X within that test, 
-    3) get an object back and take a look at all the things 
-       that X should have been called with /etc
+    Tests storage functions by mocking ipfs/ipns
     '''
 
+    @mock.patch('IPFS_CLIENT.key.gen')
+    @mock.patch('IPFS_CLIENT.cat')
+    @mock.patch('IPFS_CLIENT.name.resolve')
+    @mock.patch('IPFS_CLIENT.name.publish')
     @mock.patch('IPFS_CLIENT.add_bytes')
-    def test_upload(self, mock_ipfs_add_bytes):
+    def test_ipns(self, mock_ipfs_add_bytes, mock_ipfs_publish, mock_ipns_resolve, mock_ipfs_cat, mock_ipfs_cat):
         """
-        Tests that upload works, replace ipfs fn
+        Test storage: upload, download, create_new_ipns_link, etc
         """
-        
-    >>> credentials = {
-    ... 	"gas_payer": "0x1413862C2B7054CDbfdc181B83962CB0FC11fD92",
-    ... 	"gas_payer_priv": "28e516f1e2f99e96a48a23cea1f94ee5f073403a1c68e818263f0eb898f1c8e5"
-    ... }
-    >>> pub_key = b"2dbc2c2c86052702e7c219339514b2e8bd4687ba1236c478ad41b43330b08488c12c8c1797aa181f3a4596a1bd8a0c18344ea44d6655f61fa73e56e743f79e0d"
-    >>> job = Job(credentials=credentials, escrow_manifest=manifest)
-    >>> (hash_, manifest_url) = upload(job.serialized_manifest, pub_key)
-    >>> manifest_dict = download(manifest_url, job.gas_payer_priv)
-    >>> manifest_dict == job.serialized_manifest
-    True
-        
+        credentials = {
+            "gas_payer": "0x1413862C2B7054CDbfdc181B83962CB0FC11fD92",
+            "gas_payer_priv": "28e516f1e2f99e96a48a23cea1f94ee5f073403a1c68e818263f0eb898f1c8e5"
+        }
+        pub_key = b"2dbc2c2c86052702e7c219339514b2e8bd4687ba1236c478ad41b43330b08488c12c8c1797aa181f3a4596a1bd8a0c18344ea44d6655f61fa73e56e743f79e0d"
+        job = Job(credentials=credentials, escrow_manifest=manifest)
+        name = 'abc'
 
-        def upload(msg: Dict, public_key: bytes, ipns_keypair_name: str) -> Tuple[str, str]:
-        upload({})
-        self.assertTrue(mock_ipfs_add_bytes.called)
-        data = mock_ipfs_add_bytes.call_args[0][0].to_primitive()
+        ipns_url = create_new_ipns_link('name')
 
-        self.assertIn('job_id', data)
+        (hash_, manifest_url) = upload(job.serialized_manifest, pub_key, name)
+        manifest_dict         = download(None, job.gas_payer_priv, name)
 
-
-    @mock.patch('download')
-    def test_download(self, mock_download):
-        download
-        
-    '''
-    def download(key: str, private_key: bytes) -> Dict:
-    def create_new_ipns_link(name: str) -> str:
-    def get_ipns_link(name: str) -> str:
-    def upload(msg: Dict, public_key: bytes, ipns_keypair_name: str='') -> Tuple[str, str]:
-    '''
-
+        self.assertTrue(manifest_dict == job.serialized_manifest)
+        self.assertTrue(ipns_link_exists(name))
+        self.assertTrue(ipns_url == get_ipns_link(name))
