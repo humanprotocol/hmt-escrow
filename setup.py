acf3b288ebd0a3ba658520e3fb25af8b61ff1531
Nick Geoca <nickgeoca@gmail.com>
2019-12-18 15:16:30 -0800

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="hmt-escrow",
-    version="0.6.0",
+    version="0.6.1",
     author="HUMAN Protocol",
     description=
     "A python library to launch escrow contracts to the HUMAN network.",
