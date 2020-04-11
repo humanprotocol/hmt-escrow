e09d940ca84c544ca62e4fffaea719f3f4c27a56
Robert Valta <robert.j.valta@gmail.com>
2020-04-11 19:47:38 +0300

    Fix typo

--- hmt_escrow/storage.py
+++ hmt_escrow/storage.py
@@ -107,7 +107,7 @@ def upload(msg: Dict, public_key: bytes) -> Tuple[str, str]:
     hash_ = hashlib.sha1(manifest_.encode('utf-8')).hexdigest()
     try:
         encrypted_msg = _encrypt(public_key, manifest_)
-        key = IPFS_CLIENT.add_bytes(encryptd_msg)
+        key = IPFS_CLIENT.add_bytes(encrypted_msg)
         LOG.info("The key is {!r}, encrypted message {!r}".format(key, encrypted_msg))
     except Exception as e:
         LOG.warning("Adding bytes with IPFS failed because of: {}".format(e))
