68da30bfa28d0ebdb9024f71c63b13f76ffa8c2d
Robert Valta <robert.j.valta@gmail.com>
2020-05-07 15:28:24 +0300

    Add boto3 to required packages, bump to 0.7.16

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="hmt-escrow",
-    version="0.7.15",
+    version="0.7.16",
     author="HUMAN Protocol",
     description=
     "A python library to launch escrow contracts to the HUMAN network.",
@@ -16,5 +16,5 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
         "ipfshttpclient==0.4.13.2", "py-evm==0.2.0a37", "py-solc==3.2.0",
-        "web3==4.8.3", "hmt-basemodels"
+        "web3==4.8.3", "hmt-basemodels", "boto3==1.13.3"
     ])
