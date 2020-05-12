282b5ddab7273d17086ffd42089b100bc24023fd
Robert Valta <robert.j.valta@gmail.com>
2020-05-12 15:23:47 +0300

    Bump to 0.7.18

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="hmt-escrow",
-    version="0.7.17",
+    version="0.7.18",
     author="HUMAN Protocol",
     description=
     "A python library to launch escrow contracts to the HUMAN network.",
