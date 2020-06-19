b22ea5bfdfff95359dd03b65040d2bd4290fbe2c
Alex Newman <posix4e@gmail.com>
2020-06-19 15:43:04 -0400

    upgrade to new solcx
    Co-authored by: Alex Newman <posix4e@gmail.com>
    Co-authored by: Vlad Silviu Farcas <vladsilviufarcas@gmail.com>

--- setup.py
+++ setup.py
@@ -2,19 +2,17 @@ import setuptools
 
 setuptools.setup(
     name="hmt-escrow",
-    version="0.7.20",
+    version="0.8.0",
     author="HUMAN Protocol",
-    description=
-    "A python library to launch escrow contracts to the HUMAN network.",
+    description="A python library to launch escrow contracts to the HUMAN network.",
     url="https://github.com/hCaptcha/hmt-escrow",
     include_package_data=True,
     zip_safe=True,
     classifiers=[
         "Intended Audience :: Developers",
-        "Operating System :: OS Independent", "Programming Language :: Python"
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
     ],
-    packages=setuptools.find_packages(),
-    install_requires=[
-        "ipfshttpclient==0.4.13.2", "py-evm==0.2.0a37", "py-solc==3.2.0",
-        "web3==4.8.3", "hmt-basemodels", "boto3>=1.7.0"
-    ])
+    packages=setuptools.find_packages() + ["contracts", "migrations"],
+    install_requires=["py-solc-x", "web3", "hmt-basemodels", "boto3"],
+)
