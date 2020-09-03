92e73f4202e14368ac04b1e85f2988fbed689fb5
uivlis <vladsilviufarcas@gmail.com>
2020-09-03 14:55:44 +0300

    Fix infura uris, add new deployed address, fix saving new address to file

--- truffle.js
+++ truffle.js
@@ -14,20 +14,20 @@ module.exports = {
       network_id: '*'
     },
     live: {
-      provider: () => new HDWalletProvider(MNEMONIC, `https://mainnet.infura.io/${INFURA_TOKEN}`),
+      provider: () => new HDWalletProvider(MNEMONIC, `https://mainnet.infura.io/v3/${INFURA_TOKEN}`),
       network_id: '1',
     },
     kovan: {
-      provider: () => new HDWalletProvider(MNEMONIC, `https://kovan.infura.io/${INFURA_TOKEN}`),
+      provider: () => new HDWalletProvider(MNEMONIC, `https://kovan.infura.io/v3/${INFURA_TOKEN}`),
       network_id: '2',
     },
     ropsten: {
-      provider: () => new HDWalletProvider(MNEMONIC, `https://ropsten.infura.io/${INFURA_TOKEN}`),
+      provider: () => new HDWalletProvider(MNEMONIC, `https://ropsten.infura.io/v3/${INFURA_TOKEN}`),
       network_id: '3',
       gas: 4700000,
     },
     rinkeby: {
-      provider: () => new HDWalletProvider(MNEMONIC, `https://rinkeby.infura.io/${INFURA_TOKEN}`),
+      provider: () => new HDWalletProvider(MNEMONIC, `https://rinkeby.infura.io/v3/${INFURA_TOKEN}`),
       network_id: '4',
     },
   },
