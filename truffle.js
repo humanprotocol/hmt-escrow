ff3d01d4fa51a663d6497e75e74fe141f12f8fdc
Raphael ABADIE <rafa.abadie@gmail.com>
2022-05-13 12:03:57 +0200

    truffle-polygon-network

--- truffle.js
+++ truffle.js
@@ -1,18 +1,17 @@
-require('dotenv').config()
+require('dotenv').config();
 
 const { INFURA_TOKEN, MNEMONIC, PRIV_KEY, ETH_HOST, ETH_PORT } = process.env;
-const HDWalletProvider = require('truffle-hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 const PrivateKeyProvider = require('./private-provider');
 
 module.exports = {
-
-  plugins: ["truffle-security, solidity-coverage"],
+  plugins: ['truffle-security, solidity-coverage'],
 
   networks: {
     development: {
       host: ETH_HOST || '127.0.0.1',
       port: ETH_PORT || 8545,
-      network_id: '*'
+      network_id: '*',
     },
     live: {
       provider: () => new HDWalletProvider(MNEMONIC, `https://mainnet.infura.io/v3/${INFURA_TOKEN}`, 0, 10),
@@ -31,7 +30,7 @@ module.exports = {
       network_id: '4',
       networkCheckTimeout: '100000',
       gas: 0x989680,
-      gasPrice: 200000000000
+      gasPrice: 200000000000,
     },
     moonbase: {
       provider: () => new PrivateKeyProvider(PRIV_KEY, 'https://rpc.testnet.moonbeam.network', 1287),
@@ -41,18 +40,33 @@ module.exports = {
       provider: () => new HDWalletProvider(MNEMONIC, 'https://humanprotocol-integration.skale.network', 0, 10),
       network_id: '344435',
     },
+    mumbai: {
+      provider: () => new HDWalletProvider(MNEMONIC, `https://polygon-mumbai.infura.io/v3/${INFURA_TOKEN}`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+    },
+    polygon: {
+      provider: () => new HDWalletProvider(MNEMONIC, `https://polygon-mainnet.infura.io/v3/${INFURA_TOKEN}`),
+      network_id: 137,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      // check gasPrice here https://polygonscan.com/gastracker for deployment
+    },
   },
   compilers: {
     solc: {
-      version: "0.6.2", // Fetch exact version from solc-bin (default: truffle's version)
+      version: '0.6.2', // Fetch exact version from solc-bin (default: truffle's version)
       settings: {
         // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
           enabled: true,
-          runs: 200
+          runs: 200,
         },
-        evmVersion: "constantinople"
-      }
-    }
-  }
+        evmVersion: 'constantinople',
+      },
+    },
+  },
 };
