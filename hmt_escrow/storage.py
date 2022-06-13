47b324ef98e641a943bc1bd5a8ceea38fbff8ff2
Hugo Seabra <hugoseabra19@gmail.com>
2022-06-13 04:59:26 -0300

    feat: adding option to upload data encrypted or plain
    IMI-2282

--- hmt_escrow/storage.py
+++ hmt_escrow/storage.py
@@ -1,18 +1,16 @@
-import codecs
 import hashlib
 import json
 import logging
 import os
-import unittest
-from typing import Dict, Tuple
+from typing import Dict, Optional, Tuple
 
 import boto3
-from eth_keys import keys
 
 from hmt_escrow import crypto
 
 SHARED_MAC_DATA: bytes = os.getenv(
-    "SHARED_MAC", "9da0d3721774843193737244a0f3355191f66ff7321e83eae83f7f746eb34350"
+    "SHARED_MAC",
+    "9da0d3721774843193737244a0f3355191f66ff7321e83eae83f7f746eb34350"
 ).encode("ascii")
 
 logging.getLogger("boto").setLevel(logging.INFO)
@@ -25,7 +23,8 @@ LOG.setLevel(logging.DEBUG if DEBUG else logging.INFO)
 
 ESCROW_BUCKETNAME = os.getenv("ESCROW_BUCKETNAME", "escrow-results")
 ESCROW_AWS_ACCESS_KEY_ID = os.getenv("ESCROW_AWS_ACCESS_KEY_ID", "minio")
-ESCROW_AWS_SECRET_ACCESS_KEY = os.getenv("ESCROW_AWS_SECRET_ACCESS_KEY", "minio123")
+ESCROW_AWS_SECRET_ACCESS_KEY = os.getenv("ESCROW_AWS_SECRET_ACCESS_KEY",
+                                         "minio123")
 ESCROW_AWS_REGION = os.getenv("ESCROW_AWS_REGION", "us-west-2")
 ESCROW_ENDPOINT_URL = os.getenv("ESCROW_ENDPOINT_URL", "http://minio:9000")
 
@@ -44,6 +43,15 @@ def _connect_s3():
         raise e
 
 
+def download_from_storage(key: str) -> bytes:
+    """ Downloads data from storage if exists. """
+    LOG.debug("Downloading s3 key: {}".format(key))
+
+    BOTO3_CLIENT = _connect_s3()
+    response = BOTO3_CLIENT.get_object(Bucket=ESCROW_BUCKETNAME, Key=key)
+    return response["Body"].read()
+
+
 def download(key: str, private_key: bytes) -> Dict:
     """Download a key, decrypt it, and output it as a binary string.
 
@@ -51,8 +59,8 @@ def download(key: str, private_key: bytes) -> Dict:
     ... 	"gas_payer": "0x1413862C2B7054CDbfdc181B83962CB0FC11fD92",
     ... 	"gas_payer_priv": "28e516f1e2f99e96a48a23cea1f94ee5f073403a1c68e818263f0eb898f1c8e5"
     ... }
-    >>> from test_manifest import manifest
-    >>> from job import Job
+    >>> from test.hmt_escrow.utils import manifest
+    >>> from hmt_escrow.job import Job
     >>> pub_key = b"2dbc2c2c86052702e7c219339514b2e8bd4687ba1236c478ad41b43330b08488c12c8c1797aa181f3a4596a1bd8a0c18344ea44d6655f61fa73e56e743f79e0d"
     >>> job = Job(credentials=credentials, escrow_manifest=manifest)
     >>> (hash_, manifest_url) = upload(job.serialized_manifest, pub_key)
@@ -78,11 +86,11 @@ def download(key: str, private_key: bytes) -> Dict:
 
     """
     try:
-        LOG.debug("Downloading s3 key: {}".format(key))
-        BOTO3_CLIENT = _connect_s3()
-        response = BOTO3_CLIENT.get_object(Bucket=ESCROW_BUCKETNAME, Key=key)
-        ciphertext = response["Body"].read()
-        msg = _decrypt(private_key, ciphertext)
+        content = download_from_storage(key)
+
+        if crypto.is_encrypted(content) is True:
+            content = crypto.decrypt(private_key, content)
+
     except Exception as e:
         LOG.warning(
             "Reading the key {!r} with private key {!r} with S3 failed because of: {!r}".format(
@@ -90,16 +98,18 @@ def download(key: str, private_key: bytes) -> Dict:
             )
         )
         raise e
-    return json.loads(msg)
+    return json.loads(content)
 
 
-def upload(msg: Dict, public_key: bytes) -> Tuple[str, str]:
+def upload(msg: Dict,
+           public_key: bytes,
+           encrypt_data: Optional[bool] = False) -> Tuple[str, str]:
     """Upload and encrypt a string for later retrieval.
     This can be manifest files, results, or anything that's been already
     encrypted.
 
-    >>> from test_manifest import manifest
-    >>> from job import Job
+    >>> from test.hmt_escrow.utils import manifest
+    >>> from hmt_escrow.job import Job
     >>> credentials = {
     ... 	"gas_payer": "0x1413862C2B7054CDbfdc181B83962CB0FC11fD92",
     ... 	"gas_payer_priv": "28e516f1e2f99e96a48a23cea1f94ee5f073403a1c68e818263f0eb898f1c8e5"
@@ -122,6 +132,7 @@ def upload(msg: Dict, public_key: bytes) -> Tuple[str, str]:
     Args:
         msg (Dict): The message to upload and encrypt.
         public_key (bytes): The public_key to encrypt the file for.
+        encrypt_data (bool): Whether data must be encrypted before uploading.
 
     Returns:
         Tuple[str, str]: returns the contents of the filename which was previously uploaded.
@@ -131,127 +142,21 @@ def upload(msg: Dict, public_key: bytes) -> Tuple[str, str]:
 
     """
     try:
-        manifest_ = json.dumps(msg, sort_keys=True)
+        content = json.dumps(msg, sort_keys=True)
     except Exception as e:
         LOG.error("Can't extract the json from the dict")
         raise e
 
-    hash_ = hashlib.sha1(manifest_.encode("utf-8")).hexdigest()
+    hash_ = hashlib.sha1(content.encode("utf-8")).hexdigest()
+    key = f"s3{hash_}"
+
+    if encrypt_data:
+        content = crypto.encrypt(public_key, content)
 
     BOTO3_CLIENT = _connect_s3()
-    encrypted_msg = _encrypt(public_key, manifest_)
-    key = f"s3{hash_}"
-    BOTO3_CLIENT.put_object(Bucket=ESCROW_BUCKETNAME, Key=key, Body=encrypted_msg)
+    BOTO3_CLIENT.put_object(Bucket=ESCROW_BUCKETNAME,
+                            Key=key,
+                            Body=content)
+
     LOG.debug(f"Uploaded to S3, key: {key}")
     return hash_, key
-
-
-def _decrypt(private_key: bytes, msg: bytes) -> str:
-    """Use ECIES to decrypt a message with a given private key and an optional MAC.
-
-    >>> from test_manifest import manifest
-    >>> from job import Job
-    >>> priv_key = "28e516f1e2f99e96a48a23cea1f94ee5f073403a1c68e818263f0eb898f1c8e5"
-    >>> pub_key = b"2dbc2c2c86052702e7c219339514b2e8bd4687ba1236c478ad41b43330b08488c12c8c1797aa181f3a4596a1bd8a0c18344ea44d6655f61fa73e56e743f79e0d"
-    >>> msg = "test"
-    >>> _decrypt(priv_key, _encrypt(pub_key, msg)) == msg
-    True
-
-    Using a wrong public key to decrypt a message results in failure.
-    >>> false_pub_key = b"74c81fe41b30f741b31185052664a10c3256e2f08bcfb20c8f54e733bef58972adcf84e4f5d70a979681fd39d7f7847d2c0d3b5d4aead806c4fec4d8534be114"
-    >>> _decrypt(priv_key, _encrypt(false_pub_key, msg)) == msg
-    Traceback (most recent call last):
-    crypto.DecryptionError: Failed to verify tag
-
-    Args:
-        private_key (bytes): The private_key to decrypt the message with.
-        msg (bytes): The message to be decrypted.
-
-    Returns:
-        str: returns the plaintext equivalent to the originally encrypted one.
-
-    """
-    priv_key = keys.PrivateKey(codecs.decode(private_key, "hex"))
-    e = crypto.decrypt(msg, priv_key, shared_mac_data=SHARED_MAC_DATA)
-    return e.decode("utf-8")
-
-
-def _encrypt(public_key: bytes, msg: str) -> bytes:
-    """Use ECIES to encrypt a message with a given public key and optional MAC.
-    >>> from test_manifest import manifest
-    >>> from job import Job
-    >>> priv_key = "28e516f1e2f99e96a48a23cea1f94ee5f073403a1c68e818263f0eb898f1c8e5"
-    >>> pub_key = b"2dbc2c2c86052702e7c219339514b2e8bd4687ba1236c478ad41b43330b08488c12c8c1797aa181f3a4596a1bd8a0c18344ea44d6655f61fa73e56e743f79e0d"
-    >>> msg = "test"
-    >>> _decrypt(priv_key, _encrypt(pub_key, msg)) == msg
-    True
-
-    Args:
-        public_key (bytes): The public_key to encrypt the message with.
-        msg (str): The message to be encrypted.
-
-    Returns:
-        bytes: returns the cryptotext encrypted with the public key.
-
-    """
-    pub_key = keys.PublicKey(codecs.decode(public_key, "hex"))
-    msg_bytes = msg.encode("utf-8")
-    return crypto.encrypt(msg_bytes, pub_key, shared_mac_data=SHARED_MAC_DATA)
-
-
-class StorageTest(unittest.TestCase):
-    def test_download(self):
-        credentials = {
-            "gas_payer": "0x1413862C2B7054CDbfdc181B83962CB0FC11fD92",
-            "gas_payer_priv": "28e516f1e2f99e96a48a23cea1f94ee5f073403a1c68e818263f0eb898f1c8e5",
-        }
-        pub_key = b"2dbc2c2c86052702e7c219339514b2e8bd4687ba1236c478ad41b43330b08488c12c8c1797aa181f3a4596a1bd8a0c18344ea44d6655f61fa73e56e743f79e0d"
-        job = Job(credentials=credentials, escrow_manifest=manifest)
-        (_, manifest_url) = upload(job.serialized_manifest, pub_key)
-        manifest_dict = download(manifest_url, job.gas_payer_priv)
-        self.assertEqual(manifest_dict, job.serialized_manifest)
-
-        job = Job(credentials=credentials, escrow_manifest=manifest)
-        (_, manifest_url) = upload(job.serialized_manifest, pub_key)
-        manifest_dict = download(manifest_url, job.gas_payer_priv)
-        self.assertEqual(manifest_dict, job.serialized_manifest)
-
-    def test_upload(self):
-        credentials = {
-            "gas_payer": "0x1413862C2B7054CDbfdc181B83962CB0FC11fD92",
-            "gas_payer_priv": "28e516f1e2f99e96a48a23cea1f94ee5f073403a1c68e818263f0eb898f1c8e5",
-        }
-        pub_key = b"2dbc2c2c86052702e7c219339514b2e8bd4687ba1236c478ad41b43330b08488c12c8c1797aa181f3a4596a1bd8a0c18344ea44d6655f61fa73e56e743f79e0d"
-        job = Job(credentials=credentials, escrow_manifest=manifest)
-        (_, manifest_url) = upload(job.serialized_manifest, pub_key)
-        manifest_dict = download(manifest_url, job.gas_payer_priv)
-        self.assertEqual(manifest_dict, job.serialized_manifest)
-
-        job = Job(credentials=credentials, escrow_manifest=manifest)
-        (_, manifest_url) = upload(job.serialized_manifest, pub_key)
-        self.assertTrue(manifest_url.startswith("s3"))
-        manifest_dict = download(manifest_url, job.gas_payer_priv)
-        self.assertEqual(manifest_dict, job.serialized_manifest)
-
-    def test_decrypt(self):
-        priv_key = "28e516f1e2f99e96a48a23cea1f94ee5f073403a1c68e818263f0eb898f1c8e5"
-        pub_key = b"2dbc2c2c86052702e7c219339514b2e8bd4687ba1236c478ad41b43330b08488c12c8c1797aa181f3a4596a1bd8a0c18344ea44d6655f61fa73e56e743f79e0d"
-        msg = "test"
-        self.assertEqual(_decrypt(priv_key, _encrypt(pub_key, msg)), msg)
-        # Using a wrong public key to decrypt a message results in failure.
-        false_pub_key = b"74c81fe41b30f741b31185052664a10c3256e2f08bcfb20c8f54e733bef58972adcf84e4f5d70a979681fd39d7f7847d2c0d3b5d4aead806c4fec4d8534be114"
-        with self.assertRaises(crypto.DecryptionError):
-            _decrypt(priv_key, _encrypt(false_pub_key, msg)) == msg
-
-    def test_encrypt(self):
-        priv_key = "28e516f1e2f99e96a48a23cea1f94ee5f073403a1c68e818263f0eb898f1c8e5"
-        pub_key = b"2dbc2c2c86052702e7c219339514b2e8bd4687ba1236c478ad41b43330b08488c12c8c1797aa181f3a4596a1bd8a0c18344ea44d6655f61fa73e56e743f79e0d"
-        msg = "test"
-        self.assertEqual(_decrypt(priv_key, _encrypt(pub_key, msg)), msg)
-
-
-if __name__ == "__main__":
-    from test_manifest import manifest
-    from job import Job
-
-    unittest.main(exit=True)
--- test/hmt_escrow/test_storage.py
+++ test/hmt_escrow/test_storage.py
@@ -2,11 +2,10 @@ import logging
 import os
 import unittest
 
-from hmt_escrow import crypto
-from hmt_escrow.storage import upload, download, _encrypt, _decrypt
+from hmt_escrow.crypto import is_encrypted
 from hmt_escrow.job import Job
-
-from test.htm_escrow.utils import manifest
+from hmt_escrow.storage import upload, download, download_from_storage
+from test.hmt_escrow.utils import manifest
 
 SHARED_MAC_DATA: bytes = os.getenv(
     "SHARED_MAC",
@@ -52,21 +51,26 @@ class StorageTest(unittest.TestCase):
         manifest_dict = download(manifest_url, job.gas_payer_priv)
         self.assertEqual(manifest_dict, job.serialized_manifest)
 
-    def test_decrypt(self):
-        priv_key = "28e516f1e2f99e96a48a23cea1f94ee5f073403a1c68e818263f0eb898f1c8e5"
+    def test_upload_with_encryption_option(self):
+        """
+        Tests whether data must be persisted in storage encrypted or plain.
+        """
         pub_key = b"2dbc2c2c86052702e7c219339514b2e8bd4687ba1236c478ad41b43330b08488c12c8c1797aa181f3a4596a1bd8a0c18344ea44d6655f61fa73e56e743f79e0d"
-        msg = "test"
-        self.assertEqual(_decrypt(priv_key, _encrypt(pub_key, msg)), msg)
-        # Using a wrong public key to decrypt a message results in failure.
-        false_pub_key = b"74c81fe41b30f741b31185052664a10c3256e2f08bcfb20c8f54e733bef58972adcf84e4f5d70a979681fd39d7f7847d2c0d3b5d4aead806c4fec4d8534be114"
-        with self.assertRaises(crypto.DecryptionError):
-            _decrypt(priv_key, _encrypt(false_pub_key, msg)) == msg
+        serialized = dict(manifest.serialize())
 
-    def test_encrypt(self):
-        priv_key = b"28e516f1e2f99e96a48a23cea1f94ee5f073403a1c68e818263f0eb898f1c8e5"
-        pub_key = b"2dbc2c2c86052702e7c219339514b2e8bd4687ba1236c478ad41b43330b08488c12c8c1797aa181f3a4596a1bd8a0c18344ea44d6655f61fa73e56e743f79e0d"
-        msg = "test"
-        self.assertEqual(_decrypt(priv_key, _encrypt(pub_key, msg)), msg)
+        # Uplaoding data flagging to send it plain (not encrypted).
+        (_, manifest_url) = upload(serialized,
+                                   pub_key,
+                                   encrypt_data=False)
+        data = download_from_storage(manifest_url)
+        self.assertEqual(is_encrypted(data), False)
+
+        # Uplaoding data flagging to send it encrypted.
+        (_, manifest_url) = upload(serialized,
+                                   pub_key,
+                                   encrypt_data=True)
+        data = download_from_storage(manifest_url)
+        self.assertEqual(is_encrypted(data), True)
 
 
 if __name__ == "__main__":
