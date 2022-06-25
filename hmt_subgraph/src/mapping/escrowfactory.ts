74335d596c552c2ef37868b59f6613eecf60fe4e
Charlie85270 <rabiller.charlie@gmail.com>
2022-06-25 09:20:10 -0400

    Integrate subgraph repo to htm_escrow

---
+++ hmt_subgraph/src/mapping/escrowfactory.ts
@@ -0,0 +1,16 @@
+import { Launched } from "../../generated/EscrowFactory/EscrowFactory";
+import { LaunchedEscrow } from "../../generated/schema";
+import { Escrow } from "../../generated/templates";
+
+export function handleLaunched(event: Launched): void {
+  // Entities only exist after they have been saved to the store;
+  let entity = new LaunchedEscrow(event.params.escrow.toHex());
+
+  // Entity fields can be set based on event parameters
+  entity.eip20 = event.params.eip20;
+  entity.from = event.transaction.from;
+
+  // Entities can be written to the store with `.save()`
+  entity.save();
+  Escrow.create(event.params.escrow);
+}
