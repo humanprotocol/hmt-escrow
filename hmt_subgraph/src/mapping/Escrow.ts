efa59a464c0de6f807b8fd5ffb8829f83060fbe0
vkomodey <vkomodey@gmail.com>
2022-07-07 14:11:49 +0200

    Graph: Added Statistics entity for Escrow contract
    It includes amounts of next events:
    * IntermediateStorage
    * Pending
    * BulkTransfer

--- hmt_subgraph/src/mapping/Escrow.ts
+++ hmt_subgraph/src/mapping/Escrow.ts
@@ -4,7 +4,19 @@ import {
   IntermediateStorage,
   Pending,
 } from "../../generated/templates/Escrow/Escrow";
-import { BulkTransferEvent, ISEvent, PEvent } from "../../generated/schema";
+import { BulkTransferEvent, ISEvent, PEvent, EscrowStatistics } from "../../generated/schema";
+
+export const STATISTICS_ENTITY_ID = "escrow-statistics-id";
+
+function constructStatsEntity(): EscrowStatistics {
+  const entity = new EscrowStatistics(STATISTICS_ENTITY_ID);
+
+  entity.intermediateStorageEventCount = BigInt.fromI32(0);
+  entity.pendingEventCount = BigInt.fromI32(0);
+  entity.bulkTransferEventCount = BigInt.fromI32(0);
+
+  return entity;
+}
 
 export function handleIntermediateStorage(event: IntermediateStorage): void {
   // Entities can be loaded from the store using a string ID; this ID
@@ -21,22 +33,30 @@ export function handleIntermediateStorage(event: IntermediateStorage): void {
     entity.count = BigInt.fromI32(0);
   }
 
-  // BigInt and BigDecimal math are supported
-  // @ts-ignore
+  // BigInt and BigDecimal math are supported // @ts-ignore
   entity.count = entity.count + BigInt.fromI32(1);
 
   // Entity fields can be set based on event parameters
   entity._url = event.params._url;
   entity._hash = event.params._hash;
 
-  // Entities can be written to the store with `.save()`
   entity.save();
+
+  let statsEntity = EscrowStatistics.load(STATISTICS_ENTITY_ID);
+
+  if (!statsEntity) {
+    statsEntity = constructStatsEntity();
+  }
+
+  statsEntity.intermediateStorageEventCount += BigInt.fromI32(1);
+
+  statsEntity.save();
 }
 
 export function handlePending(event: Pending): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let id = event.transaction.from.toHex() + event.address.toHex();
+  const id = event.transaction.from.toHex() + event.address.toHex();
   let entity = PEvent.load(id);
 
   // Entities only exist after they have been saved to the store;
@@ -55,8 +75,17 @@ export function handlePending(event: Pending): void {
   entity._url = event.params.manifest;
   entity._hash = event.params.hash;
 
-  // Entities can be written to the store with `.save()`
   entity.save();
+
+  let statsEntity = EscrowStatistics.load(STATISTICS_ENTITY_ID);
+
+  if (!statsEntity) {
+    statsEntity = constructStatsEntity();
+  }
+
+  statsEntity.pendingEventCount += BigInt.fromI32(1);
+
+  statsEntity.save();
 }
 
 export function handleBulkTransfer(event: BulkTransfer): void {
@@ -80,4 +109,14 @@ export function handleBulkTransfer(event: BulkTransfer): void {
   entity.transaction = event.transaction.hash;
 
   entity.save();
+
+  let statsEntity = EscrowStatistics.load(STATISTICS_ENTITY_ID);
+
+  if (!statsEntity) {
+    statsEntity = constructStatsEntity();
+  }
+
+  statsEntity.bulkTransferEventCount += BigInt.fromI32(1);
+
+  statsEntity.save();
 }
