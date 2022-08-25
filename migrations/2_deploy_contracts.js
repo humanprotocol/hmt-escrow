6cf0c0c2d9bad0f860a91af69ab6e5115c24810e
Rene Aavik <renx81@gmail.com>
2022-08-25 14:20:01 +0300

    Refactor contracts deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,29 +3,33 @@ const fs = require('fs');
 const path = require('path');
 
 const HMToken = artifacts.require('./HMToken.sol');
+const EscrowFactory = artifacts.require('./EscrowFactory.sol');
 
-const ADDRESS_OUTPUT_FILENAME = './deployed-hmtoken/hmt.address.json';
+const TOKEN_ADDRESS_OUTPUT_FILENAME = './deployed-hmtoken/hmt.address.json';
+const FACTORY_ADDRESS_OUTPUT_FILENAME = './deployed-escrow-factory/escrow-factory.address.json';
 
-module.exports = (deployer) => {
-  deployer
-    .deploy(HMToken, 1000000000, 'Human Token', 18, 'HMT')
-    .then(() => {
-      const fileContent = {
-        address: HMToken.address,
-      };
+const saveAddress = (address, filename) => {
+  const fileContent = { address };
 
-      try {
-        fs.mkdirSync(path.dirname(ADDRESS_OUTPUT_FILENAME));
-      } catch (err) {
-        if (err.code !== 'EEXIST') throw err;
-      }
+  try {
+    fs.mkdirSync(path.dirname(filename));
+  } catch (err) {
+    if (err.code !== 'EEXIST') throw err;
+  }
 
-      fs.writeFile(ADDRESS_OUTPUT_FILENAME, JSON.stringify(fileContent, null, 2), (err) => {
-        if (err) {
-          console.error(`unable to write address to output file: ${ADDRESS_OUTPUT_FILENAME}`);
-        } else {
-          console.log(`deployed hmt token address stored in ${ADDRESS_OUTPUT_FILENAME}`);
-        }
-      });
-    });
+  fs.writeFile(filename, JSON.stringify(fileContent, null, 2), (err) => {
+    if (err) {
+      console.error(`unable to write address to output file: ${filename}`);
+    } else {
+      console.log(`deployed contract address stored in ${filename}`);
+    }
+  });
+}
+
+module.exports = async (deployer) => {
+  await deployer.deploy(HMToken, 1000000000, 'Human Token', 18, 'HMT');
+  saveAddress(HMToken.address, TOKEN_ADDRESS_OUTPUT_FILENAME);
+
+  await deployer.deploy(EscrowFactory, HMToken.address);
+  saveAddress(EscrowFactory.address, FACTORY_ADDRESS_OUTPUT_FILENAME);
 };
