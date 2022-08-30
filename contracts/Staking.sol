e493c058430688ef00a588c1428247ecf717720d
happydreamman <happydreamman@gmail.com>
2022-08-30 16:07:13 +0000

    remove @openzeppelin dependency

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -2,9 +2,8 @@
 
 pragma solidity 0.6.2;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-// import "@openzeppelin/contracts/math/SafeMath.sol";
+import "./util/IERC20.sol";
+import "./util/Ownable.sol";
 import "./SafeMath.sol";
 
 
