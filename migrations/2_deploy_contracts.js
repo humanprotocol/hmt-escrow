484b79c9ebcc7f0cc1a2d92fdeef7be93d9ee5c9
Rene Aavik <renx81@gmail.com>
2022-09-14 17:52:09 +0300

    Revert truffle changes

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,33 +3,29 @@ const fs = require('fs');
 const path = require('path');
 
 const HMToken = artifacts.require('./HMToken.sol');
-const EscrowFactory = artifacts.require('./EscrowFactory.sol');
 
-const TOKEN_ADDRESS_OUTPUT_FILENAME = './deployed-hmtoken/hmt.address.json';
-const FACTORY_ADDRESS_OUTPUT_FILENAME = './deployed-escrow-factory/escrow-factory.address.json';
+const ADDRESS_OUTPUT_FILENAME = '/deployed-hmtoken/hmt.address.json';
 
-const saveAddress = (address, filename) => {
-  const fileContent = { address };
+module.exports = (deployer) => {
+  deployer
+    .deploy(HMToken, 1000000000, 'Human Token', 18, 'HMT')
+    .then(() => {
+      const fileContent = {
+        address: HMToken.address,
+      };
 
-  try {
-    fs.mkdirSync(path.dirname(filename));
-  } catch (err) {
-    if (err.code !== 'EEXIST') throw err;
-  }
+      try {
+        fs.mkdirSync(path.dirname(ADDRESS_OUTPUT_FILENAME));
+      } catch (err) {
+        if (err.code !== 'EEXIST') throw err;
+      }
 
-  fs.writeFile(filename, JSON.stringify(fileContent, null, 2), (err) => {
-    if (err) {
-      console.error(`unable to write address to output file: ${filename}`);
-    } else {
-      console.log(`deployed contract address stored in ${filename}`);
-    }
-  });
-}
-
-module.exports = async (deployer) => {
-  await deployer.deploy(HMToken, 1000000000, 'Human Token', 18, 'HMT');
-  saveAddress(HMToken.address, TOKEN_ADDRESS_OUTPUT_FILENAME);
-
-  await deployer.deploy(EscrowFactory, HMToken.address);
-  saveAddress(EscrowFactory.address, FACTORY_ADDRESS_OUTPUT_FILENAME);
+      fs.writeFile(ADDRESS_OUTPUT_FILENAME, JSON.stringify(fileContent, null, 2), (err) => {
+        if (err) {
+          console.error(`unable to write address to output file: ${ADDRESS_OUTPUT_FILENAME}`);
+        } else {
+          console.log(`deployed hmt token address stored in ${ADDRESS_OUTPUT_FILENAME}`);
+        }
+      });
+    });
 };
