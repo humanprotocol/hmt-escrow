459bc914e3d1da6f82bfd7a73fe1c370aa7842e3
Eugene Voronov <johnvoronov@Eugenes-MacBook-Pro.local>
2022-11-02 14:11:11 +0100

    Improved deployment script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,6 +6,7 @@ const path = require("path");
 const ADDRESS_OUTPUT_FILENAME = "../hmt.address.json";
 
 async function main() {
+  // Deploy HMT token
   const HMToken = await ethers.getContractFactory("HMToken");
   const HMTokenContract = await HMToken.deploy(
     1000000000,
@@ -14,6 +15,25 @@ async function main() {
     "HMT"
   );
   const token = await HMTokenContract.deployed();
+
+  // Deploy Escrow Factory Contract
+  const EscrowFactory = await ethers.getContractFactory("EscrowFactory");
+  const escrowFactory = await EscrowFactory.deploy(token.address);
+
+  // Deploy Staking Conract
+  const minimumStake = 1;
+  const lockPeriod = 2;
+  const Staking= await ethers.getContractFactory("Staking");
+  const staking = await Staking.deploy(token.address, escrowFactory.address, minimumStake, lockPeriod);
+
+  // Deploy Staking Conract
+  const RewardPool= await ethers.getContractFactory("RewardPool");
+  const rewardPool = await RewardPool.deploy(token.address, staking.address, 1);
+
+  console.log("HMToken Address: ", token.address)
+  console.log("Escrow Factory Address: ", escrowFactory.address)
+  console.log("Staking Address: ", staking.address)
+  console.log("Reward Pool Address: ", rewardPool.address)
 
   const fileContent = {
     address: token,
